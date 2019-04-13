85456ef737bc454fdcd5a05d8f7c6b4fcb4b1b90
Dreex <ignacio.villa.rey@gmail.com>
2019-04-13 11:13:07 -0400

    sábado 13 abril

---
+++ FuncionR.R
@@ -0,0 +1,4 @@
+source(misegundoscript)
+variableUno<-funcionUno(1,2)
+variableDos<-velocidad(10,2)
+variableTres<-algebra(2,5,6)
--- misegundoscript.R
+++ misegundoscript.R
@@ -1,7 +1,24 @@
-funcionUno<-function(){
-  ##Aquí va el desarrollo de la funcion #() poner las variables que a uno le envien
-  print("Hola Mundo")
-}
+funcionUno<-function(x,y){
+  ##Aquí va el desarrollo de la funcion de 
+  print("entró a la suma")
+  x+y
+
 funcionUno()
+}
 
-
+# Realizar el desarrollo de la velocidad de un cuerpo en relación al tiempo donde v=d/t
+#Agregar valores de entrada y salida
+#d -> double - t -> double
+#function velocidad(double,double) ->double
+#Test velocidad(10,2) ->5
+velocidad<-function(d,t){
+#Ahora se divide
+  print(d/t)
+}
+algebra<-function(a,b,c){
+  #Se multiplica los dos primeros números y dicho resultado se divide por el tercero
+  ((a*b)-((trunc((a*b)/c)*c)))
+  
+}
+round(3,4)
+trunc(3.2,0)
